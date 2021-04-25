95b6ffadc8189791ec639e17eeaab03806828dc1
YanoRyo <yanoryo7032@gmail.com>
2021-04-25 10:42:20 +0900

    投稿の詳細ページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # get '/posts/new', to:'posts#new'
   # post '/posts', to:'posts#create'
-  resources :posts,only: %i(new create index)
+  resources :posts,only: %i(new create index show)
 
 
 end
